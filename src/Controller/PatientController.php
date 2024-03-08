169df0049c7b1472b07c12542383d57efd33748a
Sylvain Regnier <regnier.sylvain@yahoo.fr>
2024-03-08 11:53:57 +0100

    edit

--- src/Controller/PatientController.php
+++ src/Controller/PatientController.php
@@ -3,80 +3,45 @@
 namespace App\Controller;
 
 use App\Entity\Patient;
-use App\Form\PatientCrudType;
-use App\Repository\PatientRepository;
+use App\Form\PatientType;
+
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Bundle\SecurityBundle\Security;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-#[Route('/patient')]
 class PatientController extends AbstractController
 {
-    #[Route('/', name: 'app_patient_index', methods: ['GET'])]
-    public function index(PatientRepository $patientRepository): Response
+    #[Route('/patient/profil', name: 'app_patient_profil')]
+    public function update(EntityManagerInterface $entityManager,Security $security): Response
     {
+
+        $user = $security->getUser();
+        $userId=$user->getId();
+  
+        if($userId){
+       
+            $edit = $entityManager->getRepository(Patient::class)->findOneBy(['user' => $userId]);
+         
+            $form = $this->createForm(PatientType::class,$edit);
+            if(!$edit){
+                throw $this->createNotFoundException(
+                    'Personne de trouver '
+                );
+            }
+  
+
+            return $this->render('patient/index.html.twig',[
+                'form' => $form->createView(),
+            ]);
+
+        }
+
+
+
         return $this->render('patient/index.html.twig', [
-            'patients' => $patientRepository->findAll(),
+            'controller_name' => 'PatientController',
         ]);
-    }
-
-    #[Route('/new', name: 'app_patient_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, EntityManagerInterface $entityManager): Response
-    {
-        $patient = new Patient();
-        $form = $this->createForm(PatientCrudType::class, $patient);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->persist($patient);
-            $entityManager->flush();
-
-            return $this->redirectToRoute('app_patient_index', [], Response::HTTP_SEE_OTHER);
-        }
-
-        return $this->render('patient/new.html.twig', [
-            'patient' => $patient,
-            'form' => $form,
-        ]);
-    }
-
-    #[Route('/{id}', name: 'app_patient_show', methods: ['GET'])]
-    public function show(Patient $patient): Response
-    {
-        return $this->render('patient/show.html.twig', [
-            'patient' => $patient,
-        ]);
-    }
-
-    #[Route('/{id}/edit', name: 'app_patient_edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, Patient $patient, EntityManagerInterface $entityManager,Security $security): Response
-    {
-        $form = $this->createForm(PatientCrudType::class, $patient);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->flush();
-
-            return $this->redirectToRoute('app_patient_index', [], Response::HTTP_SEE_OTHER);
-        }
-
-        return $this->render('patient/edit.html.twig', [
-            'patient' => $patient,
-            'form' => $form,
-        ]);
-    }
-
-    #[Route('/{id}', name: 'app_patient_delete', methods: ['POST'])]
-    public function delete(Request $request, Patient $patient, EntityManagerInterface $entityManager): Response
-    {
-        if ($this->isCsrfTokenValid('delete'.$patient->getId(), $request->request->get('_token'))) {
-            $entityManager->remove($patient);
-            $entityManager->flush();
-        }
-
-        return $this->redirectToRoute('app_patient_index', [], Response::HTTP_SEE_OTHER);
     }
 }
