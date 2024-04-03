9d5ba18c651156c7dd82279ca57f8555e74259e5
Sylvain Regnier <regnier.sylvain@yahoo.fr>
2024-04-03 15:49:38 +0200

    dp

--- src/Controller/SocieteController.php
+++ src/Controller/SocieteController.php
@@ -4,6 +4,8 @@ namespace App\Controller;
 
 use App\Entity\Ajouter;
 use App\Entity\Employer;
+use App\Entity\Rdv;
+use App\Entity\Societe;
 use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -36,4 +38,40 @@ class SocieteController extends AbstractController
             'controller_name' => 'SocieteController',
         ]);
     }
+    #[Route('/societe/employer/rdv', name: 'app_societe_employer_rdv')]
+public function rdvTousEmployer(Security $security,EntityManagerInterface $entityManager){
+$user=$security->getUser();
+$societe=$entityManager->getRepository(Societe::class)->findOneBy(['user'=>$user]);
+$employer=$entityManager->getRepository(Ajouter::class)->findBy(['societe'=>$societe]);
+// $rdv=$entityManager->getRepository(Rdv::class)->findBy(['employer'=>$employer]);
+$employerId = [];
+$rdvs = [];
+
+
+foreach ($employer as $employers) {
+
+    $employerId[] = (string) $employers->getEmployer()->getId(); 
+}
+
+
+$dateJour = new \DateTime();
+foreach ($employerId as $id) {
+   
+    $idEmployer = (string) $id;
+
+    $rdvEmployer = $entityManager->getRepository(Rdv::class)->findBy(['employer' => $idEmployer,'date_rdv'=>$dateJour,'status_rdv' => null]);
+    
+  
+        $rdvs[$idEmployer] = $rdvEmployer;
+        for ($heure = 8; $heure <= 17; $heure++) {
+         
+            $creneauxDisponibles[sprintf("%02d:00 - %02d:00", $heure, $heure + 1)] = 'Libre';
+        }
+        
+    
+}
+
+
+dd($creneauxDisponibles);
+}
 }
